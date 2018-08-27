5928283501d29bea35addc0d2d8e03b9509b181f
不老的石头 <zkanou@163.com>
2018-08-27 21:12:24 +0800

    代码重构 操纵飞船

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,8 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
+import game_functions as gf
 
 def run_game():
     #初始化游戏并创建一个屏幕对象
@@ -9,18 +11,14 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
+    #创建一艘飞船
+    ship = Ship(screen)
+
     #开始游戏主循环
     while True:
         #监视键盘和鼠标事件
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
+        gf.check_events(ship)
 
-        screen.fill(ai_settings.bg_color)
-
-        #让最近绘制的屏幕可见
-        pygame.display.flip()
-
-
+        gf.update_screen(ai_settings, screen, ship)
 
 run_game()
